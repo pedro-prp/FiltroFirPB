4a4f36af882d797d76dfd3623445c12ef506ef1c
pedro-prp <peddroprp@gmail.com>
2018-06-17 01:59:55 -0300

    basico funcionamento implemntado

--- MATLAB/Entrega1_Grupo6.m
+++ MATLAB/Entrega1_Grupo6.m
@@ -13,4 +13,19 @@ f3=30e3;
 
 y = 5*sin(2*pi*f1*dt) + 5*sin(2*pi*f2*dt) + 10*sin(2*pi*f3*dt);
 
-plot(dt,y)
+nfft = length(y);
+nfft2 = 2.^nextpow2(nfft);
+
+fy = fft(y,nfft2);
+fy= fy(1:nfft2/2);
+
+xfft=Fs.*(0:nfft2/2-1)/nfft2;
+
+cut_off=1.5e3/Fs/2;
+order=32;
+
+h=fir1(order,cut_off,'low');
+
+con = conv(y,h);
+
+plot(con);
